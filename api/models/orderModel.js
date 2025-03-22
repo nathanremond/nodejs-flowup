f3c18b5e50b45a1aeb858a5c2e43169e1bd098e7
nathanremond <perso.nathan.remond@gmail.com>
2025-03-22 16:37:52 +0100

    feat: endpoint order + correction login

--- api/models/orderModel.js
+++ api/models/orderModel.js
@@ -11,12 +11,31 @@ const pool = new Pool ({
 });
 
 class OrderModel {
-  static async create({order_date}) {
-    const result = await pool.query(
-      "INSERT INTO orders (order_date) VALUES ($1) RETURNING *",
-      [order_date]
-    );
-    return result.rows[0];
+  static async create({total_amount, ordered_products}, id_user) {
+    const client = await pool.connect();
+    try {
+      await client.query("BEGIN");
+      const order_date = new Date().toISOString().split("T")[0];
+      const orderResult = await client.query(
+        "INSERT INTO orders (order_date, total_amount, id_user) VALUES ($1, $2, $3) RETURNING *",
+        [order_date, total_amount, id_user]
+      );
+      const order = orderResult.rows[0];
+
+      for (const product of ordered_products) {
+        await client.query(
+          "INSERT INTO order_product (id_order, id_product, quantity) VALUES ($1, $2, $3)",
+          [order.id_order, product.id_product, product.quantity]
+        );
+      }
+      await client.query("COMMIT");
+      return order;
+    } catch (error) {
+      await client.query("ROLLBACK");
+      throw error;
+    } finally {
+      client.release();
+    }
   }
 }
 
