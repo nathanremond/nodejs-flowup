d032ba01ef8287a35f8c3cbc980d273b6ed31791
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2025-03-28 10:45:40 +0100

    feat: ajout de la page collaborations

---
+++ frontend/pages/brand.js
@@ -0,0 +1,45 @@
+import { useEffect, useState, useContext, use } from "react";
+import { useRouter } from "next/router";
+import AuthContext from "../context/AuthContext";
+
+export default function Collab() {
+    const { token } = useContext(AuthContext);
+    const router = useRouter();
+    const [brand, setBrand] = useState(null);
+
+    useEffect(() => {
+        fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/brand`)
+          .then((res) => res.json())
+          .then((data) => setBrand(data))
+          .catch((err) => console.error("Erreur :", err));
+      }
+      , []);
+    
+    return (
+        <div>
+            <header>
+                <div>
+                    <a href="/login" className=""> <img src="icone_de_connexion.png" alt="Icône de connexion"/> </a>
+                    <a href="/" className="text-3xl font-bold underline"> <img src="icone_de_panier.png" alt="Icône de connexion"/> </a>
+                </div>
+                <nav class="menu">
+                    <a href="/">Accueil</a>
+                    <a href="/category">PC</a>
+                    <a href="/brand">Collaborations</a>
+                    <a href="/request">PC personnalisés</a>   
+                </nav>
+            </header>
+            <main>
+                <h1>Marques</h1>
+                {brand && brand.map((brand) => (
+                    <a href="/brand/[brand.id]" className="brand">
+                        <div key={brand.id} >
+                            <h2>{brand.name}</h2>
+                            <p>{brand.description}</p>
+                        </div>
+                    </a>
+                ))}
+            </main>
+        </div>
+    )
+} 
