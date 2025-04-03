650a0ff78663188500a5db4ffc70f2e16c15a2b4
nathanremond <99425832+nathanremond@users.noreply.github.com>
2025-04-03 09:09:25 +0200

    Création header + affichage sur toutes les pages du site (#45)
    * feat: création header
    
    * feat: header sur page category
    
    * feat: header sur page brand
    
    * feat: header sur page panier
    
    * feat: header sur page product
    
    * fix: affichage images header sur toutes les pages

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,10 @@
 import { AuthProvider } from "../context/AuthContext";
+import Header from "../layout/header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Header />
       <Component {...pageProps} />
     </AuthProvider>
   );
--- frontend/pages/brand.js
+++ frontend/pages/brand.js
@@ -1,8 +1,7 @@
-import { useEffect, useState, useContext, use } from "react";
+import { useEffect, useState} from "react";
 
 export default function Collab() {
     const [brand, setBrand] = useState(null);
-    const [category, setCategory] = useState(null);
 
     useEffect(() => {
         fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/brand`)
@@ -11,35 +10,9 @@ export default function Collab() {
           .catch((err) => console.error("Erreur :", err));
       }
       , []);
-
-    useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/category`)
-        .then((res) => res.json())
-        .then((data) => setCategory(data))
-        .catch((err) => console.error("Erreur :", err));
-      }
-      , []);  
     
     return (
         <div>
-            <header>
-                <div>
-                    <a href="/login" className=""> <img src="icone_de_connexion.png" alt="Icône de connexion"/> </a>
-                    <a href="/order" className="text-3xl font-bold underline"> <img src="icone_de_panier.png" alt="Icône de panier"/> </a>
-                </div>
-                <nav class="menu">
-                    <a href="/">Accueil</a>
-                    {category && category.map((category) => (
-                        <a href={`/category/${category.id}`} className="category">
-                            <div key={category.id} >
-                                {category.name}
-                            </div>
-                        </a>
-                    ))}
-                    <a href="/brand">Collaborations</a>
-                    <a href="/request">PC personnalisés</a>   
-                </nav>
-            </header>
             <main>
                 <h1>Marques</h1>
                 {brand && brand.map((brand) => (
